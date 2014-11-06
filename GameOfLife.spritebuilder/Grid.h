f04ad18d1e37616eb39dc242ce5c42aa8be487ba
malxlip <malcolmlipscomb@Agent-14.local>
2014-11-05 22:06:56 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.h
+++ GameOfLife.spritebuilder/Grid.h
@@ -8,11 +8,13 @@
 
 #import "CCSprite.h"
 
-@interface grid : CCSprite
+@interface Grid : CCSprite
 
 @property (nonatomic, assign) int totalAlive;
 @property (nonatomic, assign) int generation;
 
-
+-(void) evolveStep;
+-(void) countNeighbors;
+-(void) updateCreatures;
 
 @end
---
+++ GameOfLife.spritebuilder/Grid.m
@@ -0,0 +1,118 @@
+//
+//  grid.m
+//  GameOfLife
+//
+//  Created by Malcolm Lipscomb on 10/29/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Grid.h"
+#import "Creature.h"
+
+static const int GRID_ROWS = 8;
+static const int GRID_COLUMNS = 10;
+
+@implementation Grid{
+    NSMutableArray *_gridArray;
+    float _cellWidth;
+    float _cellHeight;
+}
+
+-(void) onEnter{
+    [super onEnter];
+    [self setupGrid];
+
+    self.userInteractionEnabled = YES;
+}
+
+-(void) evolveStep{
+    [self countNeighbors];
+    [self updateCreatures];
+    _generation++;
+
+}
+
+-(void)countNeighbors{
+    for (int i = 0; i < [_gridArray count]; i++) {
+        for (int j = 0; j <[_gridArray[i] count]; j++) {
+            Creature *currentCreature = _gridArray[i][j];
+            currentCreature.livingNeighbors = 0;
+            for (int x = (i - 1); x <= (i + 1); x++) {
+                for (int y = (j - 1); y <= (j+1); y++) {
+                    BOOL isIndexValid;
+                    isIndexValid = [self isIndexValidForX:x andY:y];
+                    if (!((x == i) && (y == j))&& isIndexValid){
+                        Creature *neighbor = _gridArray[x][y];
+                        if (neighbor.isAlive) {
+                            currentCreature.livingNeighbors += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+-(void)updateCreatures{
+    for (int i = 0; i < [_gridArray count]; i++) {
+        for (int j = 0; j < [_gridArray[i] count]; j++) {
+            Creature *currentCreature = _gridArray[i][j];
+            if (currentCreature.livingNeighbors == 3) {
+                currentCreature.isAlive = YES;
+            }
+            else if(currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 4){
+                currentCreature.isAlive = NO;
+                
+            }
+        }
+    }
+}
+
+-(BOOL) isIndexValidForX:(int)x andY:(int)y{
+    BOOL isIndexValid = YES;
+    if (x < 0 || y < 0 || x >= GRID_ROWS || y >= GRID_COLUMNS){
+        isIndexValid = NO;
+    }
+    return isIndexValid;
+
+}
+
+-(void) setupGrid{
+    _cellWidth = self.contentSize.width / GRID_COLUMNS;
+    _cellHeight = self.contentSize.height / GRID_ROWS;
+    
+    float x = 0;
+    float y = 0;
+    
+    _gridArray = [NSMutableArray array];
+    
+    for (int i = 0; i < GRID_ROWS; i++) {
+        _gridArray[i] = [NSMutableArray array];
+        x = 0;
+        
+        for (int j = 0; j < GRID_COLUMNS; j++) {
+            Creature *creature = [[Creature alloc] initCreature];
+            creature.anchorPoint = ccp(0, 0);
+            creature.position = ccp(x,y);
+            [self addChild:creature];
+            _gridArray[i][j] = creature;
+            x+=_cellWidth;
+        }
+        
+        y+=_cellHeight;
+    }
+}
+
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
+    CGPoint touchLocation = [touch locationInNode:self];
+    Creature *creature = [self creatureForTouchPosition:touchLocation];
+    creature.isAlive = !creature.isAlive;
+}
+
+- (Creature*) creatureForTouchPosition:(CGPoint)touchPosition{
+    int row = touchPosition.y / _cellHeight;
+    int column = touchPosition.x / _cellWidth;
+    return _gridArray[row][column];
+}
+
+@end
