1663504cd3522d2fed15aa3aea9bb26c3dbd0bcb
malxlip <malcolmlipscomb@Agent-14.local>
2014-11-09 11:04:35 -0500

    Game Of Life
    Practice App

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -101,10 +101,9 @@ static const int GRID_COLUMNS = 10;
     for (int i = 0; i < [_gridArray count]; i++) {
         for (int j = 0; j < [_gridArray[i] count]; j++) {
             Creature *currentCreature = _gridArray[i][j];
-             numAlive += 1;
             if (currentCreature.livingNeighbors == 3) {
                 currentCreature.isAlive = TRUE;
-               
+                 numAlive += 1;
             }
             else if(currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >=4 ){
                 currentCreature.isAlive = FALSE;
